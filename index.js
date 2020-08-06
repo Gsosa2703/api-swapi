0f955d4818ebc2bc7589f7f8339ad11f9ea6dc7e
Gabriela Sosa <gsosa2703@gmail.com>
2020-08-06 13:42:18 -0300

    llamada a la api y recibiendo homeworld con errores

--- index.js
+++ index.js
@@ -8,5 +8,30 @@ fetch(API)
 .then(data => {
     return data.json()
 }).then(character => {
-    const titulo = document.getElementById('nombre').innerHTML = character.name;
-}) 
+    const titulo = document.getElementById('nombre').innerHTML += character.name;
+    return character
+}).then(character => {
+    fetch(homeworld.homeworld).then(response =>{
+        return  response.json()
+    }).then(response => {
+        console.log(response.name)
+        const  parrafoHomworld = document.getElementById('homeworld').innerHTML += response.name;
+    }).catch(err => {
+        console.error(err)
+    })
+
+    return character.starships
+}).then(starships => {
+    for ( let i = 0; i < starships.length; i++) {
+        fetch(starships[i]).then(response=> {
+            return response.json()
+        }).then(starship => {
+            console.log(starship)
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+    console.log(character.starships)
+}).catch(err => {
+    console.error(err)
+})
